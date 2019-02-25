3b960144445a25ec0063dd90c0e10ed79e33ebae
Katsuya Mito <kmito@cjradiance.asia>
2019-02-25 11:20:07 +0900

    fff

--- WindowsFormsApp3/Program.cs
+++ WindowsFormsApp3/Program.cs
@@ -17,6 +17,8 @@ namespace WindowsFormsApp3
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
+            //ffF
         }
     }
 }
